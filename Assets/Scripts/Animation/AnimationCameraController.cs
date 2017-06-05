594f759e5f00310e6f213f58df8ed373781220e8
Justin Tran <jtt65@cornell.edu>
2017-06-05 14:56:11 -0400

    Animation

---
+++ Assets/Scripts/Animation/AnimationCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 positionOffset;
+	Vector3 position = new Vector3();
+	
+	// Use this for initialization
+	void Start () {
+		positionOffset = transform.position - player.transform.position;
+
+		position.y = 3.0f;
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		position.x = player.transform.position.x + positionOffset.x;
+		position.z = player.transform.position.z + positionOffset.z;
+
+		transform.position = position;
+
+		//transform.position = player.transform.position + positionOffset;
+		
+		//transform.rotation = player.transform.rotation;
+
+		
+
+		
+	}
+}
---
+++ Assets/Scripts/Animation/AnimationPlayerController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationPlayerController : MonoBehaviour
+{
+    Animator anim;
+    private float speed = 0.5f;
+    
+    
+    
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    void Update(){
+
+         if (Input.GetKeyDown(KeyCode.Space)){
+            anim.SetBool("jump", true);
+        }
+        else{
+            anim.SetBool("jump", false);
+        }
+
+        if (Input.GetKey(KeyCode.W)){
+            if (Input.GetKey(KeyCode.LeftShift)){
+                 anim.SetFloat("velZ", speed*2, 1f, Time.deltaTime);
+            }
+            else{
+                anim.SetFloat("velZ", speed, 1f, Time.deltaTime);
+            }
+            anim.SetBool("walking", true);
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            anim.SetFloat("velZ", -speed);
+            anim.SetBool("walking", true);
+        }
+        else{
+            anim.SetFloat("velZ", 0); 
+            anim.SetBool("walking", false);
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+                anim.SetFloat("velX", -speed);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            anim.SetFloat("velX", speed);
+        }
+        else{
+            anim.SetFloat("velX", 0);
+        }
+
+       
+
+
+        //no only state machines
+        // if (Input.GetKeyDown(KeyCode.Space)){
+        //     anim.SetBool("jump", true);
+        // }
+        // else{
+        //     anim.SetBool("jump", false);
+        // }
+
+        // if (Input.GetKey(KeyCode.W)){
+        //     if (Input.GetKey(KeyCode.LeftShift)){
+        //         anim.SetFloat("speed", speed*2);
+        //     }
+        //     else{
+        //         anim.SetFloat("speed", speed);
+        //     }
+        // }
+        // else{
+        //     anim.SetFloat("speed", 0);
+            
+        // }
+
+        
+
+
+
+        
+       
+    }
+}
